a6c013cfaed81adae0ac9f9057be931ec597023f
Alex Sanders <alex@sndrs.dev>
2023-11-01 16:42:15 +0000

    add yarn 4 pnpm demo app

---
+++ index.mjs
@@ -0,0 +1,5 @@
+import react from "react";
+import prettier from "prettier";
+
+console.log('react@',react.version);
+console.log('prettier@',prettier.version);
---
+++ workspaces/a/index.mjs
@@ -0,0 +1,5 @@
+import react from "react";
+import b from "b/b.js"
+
+console.log('react@',react.version);
+console.log('b =',b);
